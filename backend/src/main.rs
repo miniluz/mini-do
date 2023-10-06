bd2e488c7f12747fa3f6b50015be9663f26ab57c
miniluz <javiermelon4fu@gmail.com>
2023-10-06 11:31:55 +0200

    Added migration and entity generator

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -6,13 +6,8 @@ use color_eyre::Result;
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let db = Database::connect(format!(
-        "postgres://{}:{}@db/{}",
-        env::var("POSTGRES_USER").expect("POSTGRES_USER expected in env"),
-        env::var("POSTGRES_PASSWORD").expect("POSTGRES_PASSWORD expected in env"),
-        env::var("POSTGRES_DB").expect("POSTGRES_DB expected in env"),
-    ))
-    .await?;
+    dbg!(env::var("DATABASE_URL")?);
+    let db = Database::connect(env::var("DATABASE_URL")?).await?;
 
     println!("All fine!");
 
---
+++ generate-entities.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose-generate-entity.yaml up --build
